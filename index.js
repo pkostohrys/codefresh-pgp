a476ec61bc81c45ea6bfd2e76d6a7f695834a707
pasha <pavel@codefresh.io>
2019-08-28 16:38:21 +0300

    Add decryption functionality

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const openpgp = require('openpgp');
 const glob = require('glob');
 const fs = require('fs');
 const Promise = require('bluebird');
-const pubkey = process.env.PUBLIC_KEY;
-const globExpression = process.env.GLOB;
+const yn = require('yn');
 
-async function _encryptFile(content, file){
+const decryption = yn(process.env.DECRYPT);
+const globExpression = process.env.GLOB;
+const pubkey = process.env.PUBLIC_KEY;
+const privkey = process.env.PRIVAT_KEY;
+const passphrase = process.env.PASS_PHRASE;
+
+const base64ToUtf8 = str => new Buffer(str, 'base64').toString('utf8')
+
+async function _encryptFile(content, file) {
 
     const options = {
-        publicKeys: (await openpgp.key.readArmored(new Buffer(pubkey, 'base64').toString('utf8'))).keys,
+        publicKeys: (await openpgp.key.readArmored(base64ToUtf8(pubkey))).keys,
         message: openpgp.message.fromBinary(content),
         format: 'binary'
     };
@@ -18,18 +25,46 @@ async function _encryptFile(content, file){
     });
 }
 
-const encrypt = async() => {
+async function _decryptFile(content, file) {
 
-    if(!pubkey) {
-        console.error('key should be provided');
-        process.exit(1);
-        return;
+    const formattedContent = new TextDecoder().decode(content);
+
+    const privKeyObj = (await openpgp.key.readArmored(base64ToUtf8(privkey))).keys[0]
+    privKeyObj.decrypt(passphrase);
+
+    const options = {
+        message: await openpgp.message.readArmored(formattedContent),
+        privateKeys: [privKeyObj]
     }
 
-    if(!globExpression) {
+    return openpgp.decrypt(options).then(decodedText => {
+        return Promise.fromCallback(cb => fs.writeFile(file, decodedText.data, cb));
+    });
+}
+
+const encryptDecrypt = async () => {
+    if (!globExpression) {
         console.error('files glob expression should be provided');
         process.exit(1);
         return;
+    }
+    if (decryption) {
+        if (!privkey) {
+            console.error('privat key should be provided');
+            process.exit(1);
+            return;
+        }
+        if (!passphrase) {
+            console.error('passphrase key should be provided');
+            process.exit(1);
+            return;
+        }
+    } else {
+        if (!pubkey) {
+            console.error('public key should be provided');
+            process.exit(1);
+            return;
+        }
     }
 
     const files = await Promise.fromCallback(cb => glob(globExpression, {}, cb));
@@ -39,10 +74,12 @@ const encrypt = async() => {
     }));
 
     const promises = filesContent.map(fileContent => {
-        return _encryptFile(fileContent.content, fileContent.file);
+        return decryption
+            ? _decryptFile(fileContent.content, fileContent.file)
+            : _encryptFile(fileContent.content, fileContent.file);
     });
 
     await Promise.all(promises.map(p => p.catch(() => undefined)));
 };
 
-encrypt();
+encryptDecrypt();
